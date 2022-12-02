47abfad5cbcea400ec36675496a31f2c8746bdc2
CassDurrGithub <119500637+CassDurrGithub@users.noreply.github.com>
2022-12-02 15:08:18 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='T5X Retrieval',
+      version='0.0.0',
+      description='T5X Retrieval is a JAX implementation of T5 (Text-to-Text Transfer Transformer) optimized for retrieval applications.',
+      author='Google Research',
+      author_email='no-reply@google.com',
+      license='Apache 2.0',
+      url='https://github.com/google-research/t5x_retrieval'
+     )
